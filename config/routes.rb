a5b485db62853c92b3eea5e2567d11879c9757d9
mono-glyceride <glyceride.mono@gmail.com>
2023-08-25 21:49:14 +0900

    update posts routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts
+  resources :posts, only: [:new, :create]
   namespace :posts do
     resources :areas, only: :index, default: { fortmat: :json }
   end
