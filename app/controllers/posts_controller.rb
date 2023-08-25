fd21fb15c852f21d9c78c027210e7cdcaa33288b
yu-0811 <teradayu0811@gmail.com>
2023-08-25 16:07:21 +0900

    Make create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(posts_paramas)
+    if @post.save
+      flash[:success] = '投稿しました'
+      redirect_to posts_path
+    else
+      flash.now[:denger] = '投稿に失敗しました'
+      render :new
+    end
+  end
+
   private
     def posts_paramas
+
       params.require(:post).permit(:content, :latitude, :longitude)
     end
 
