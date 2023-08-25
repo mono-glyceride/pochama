967d1d252434093f02b70f62eca30ce57d8c9673
yu-0811 <teradayu0811@gmail.com>
2023-08-25 15:12:46 +0900

    Make validate

--- db/migrate/20230825054741_create_posts.rb
+++ db/migrate/20230825054741_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :content
+      t.text :content, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
 
       t.timestamps
     end
