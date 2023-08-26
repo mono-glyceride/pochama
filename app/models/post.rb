76349a70a6f5692ffaead47923270b5d46f82280
yu-0811 <teradayu0811@gmail.com>
2023-08-26 10:45:16 +0900

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :content, length: { maximum: 200 }
+  validates :latitude, numericality: { in: 0..90 }
+  validates :longitude, numericality: { in: 0..360 }
 end
