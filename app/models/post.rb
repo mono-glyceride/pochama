39c12134871429bcd29df692eab42f7270c484ad
yu-0811 <teradayu0811@gmail.com>
2023-08-26 11:25:53 +0900

    edit validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :content, length: { maximum: 200 }
+  validates :content, presence: true, length: { maximum: 200 }
   validates :latitude, numericality: { in: 0..90 }
   validates :longitude, numericality: { in: 0..360 }
 end
