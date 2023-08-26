157065fe39a1359551d51208fc7dbaa937622528
yu-0811 <teradayu0811@gmail.com>
2023-08-26 12:21:03 +0900

    edit length

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "faker"
 
 100.times do |n|
-  sample_content = Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 300)
+  sample_content = Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 190)
   sample_latitude = Faker::Number.between(from: 20.00, to: 45.00).round(7)
   sample_longitude = Faker::Number.between(from: 120.00, to: 150.00).round(7)
   Post.create!(
@@ -12,7 +12,7 @@ require "faker"
 end
 
 10.times do |n|
-  sample_content = Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 300)
+  sample_content = Faker::Lorem.sentence(word_count: 5, supplemental: true, random_words_to_add: 190)
   sample_latitude = Faker::Number.between(from: 35.0031361-0.009, to: 35.0031361+0.009).round(7)
   sample_longitude = Faker::Number.between(from: 135.7584737-0.009, to: 135.7584737+0.009).round(7)
   Post.create!(
