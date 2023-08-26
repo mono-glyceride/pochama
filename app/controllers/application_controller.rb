381ff6388efcc7188140e58ff7998cb01206eda8
yu-0811 <teradayu0811@gmail.com>
2023-08-26 18:09:26 +0900

    add 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  
+  unless Rails.env.development?
+    rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+  end
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  private
+
+  def render_404
+    render 'error/404', status: :not_found
+  end
+
 end
