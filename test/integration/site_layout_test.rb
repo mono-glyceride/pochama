48d004dcb08e00905ce83b61a76f0f9376d7daae
yu-0811 <teradayu0811@gmail.com>
2023-08-27 14:49:01 +0900

    make test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'posts/index'
+    assert_select "a[href=?]", new_post_path
+    #assert_select "a[href=?]", map_posts_path
+  end
+
+end
