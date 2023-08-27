c474454d30d8f0535493f56baf5af935fb47cfa4
yu-0811 <teradayu0811@gmail.com>
2023-08-27 18:44:40 +0900

    edit S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
   validates :content, presence: true, length: { maximum: 200 }
   validates :latitude, numericality: { in: 0..90 }
   validates :longitude, numericality: { in: 0..360 }
