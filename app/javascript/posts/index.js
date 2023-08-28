52518b2bfa94a02992ba4ab65859bc6213bcf298
Kazuma Ito <zarigani2x@gmail.com>
2023-08-28 10:38:24 +0900

    Update

--- app/javascript/posts/index.js
+++ app/javascript/posts/index.js
@@ -3,6 +3,19 @@ import moment from "moment";
 
 console.log("hello index");
 
+moment.locale("ja", {
+  weekdays: [
+    "日曜日",
+    "月曜日",
+    "火曜日",
+    "水曜日",
+    "木曜日",
+    "金曜日",
+    "土曜日",
+  ],
+  weekdaysShort: ["日", "月", "火", "水", "木", "金", "土"],
+});
+
 var listContainer = document.querySelector("#post-list");
 
 for (let i = 0; i < 5; i++) {
@@ -59,9 +72,9 @@ navigator.geolocation.getCurrentPosition((position) => {
             </div>`
               : ""
           }
-          <a class="post-content-coords" href="<%= map_posts_path %>?id=${
-            post.id
-          }&lat=${post.latitude}&lng=${post.longitude}">
+          <a class="post-content-coords" href="/posts/map?id=${post.id}&lat=${
+          post.latitude
+        }&lng=${post.longitude}">
             <div>
               ${post.latitude}, ${post.longitude}
             </div>
